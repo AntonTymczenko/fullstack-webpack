0139e3b169742d0f59d78409120bc80ffb314ba1
Anton Tymczenko <tymczenko@protonmail.com>
2017-10-22 06:52:42 +0300

    register page

--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -2,15 +2,6 @@ import Api from '@/services/Api'
 
 export default {
   register (credentials) {
-    return Api().post('register', credentials)
+    return Api().post('/register', credentials)
   }
 }
-
-/*
-call this so:
-
-AuthenticationServices.register({
-  email: 'test@mail',
-  password: '1234'
-})
-*/
