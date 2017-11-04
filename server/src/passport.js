6c48f49f65885e85b491d8fed0996c5c8fb71882
Anton Tymczenko <tymczenko@protonmail.com>
2017-11-04 02:43:16 +0200

    building passport

--- server/src/passport.js
+++ server/src/passport.js
@@ -8,6 +8,12 @@ passport.use(
   new JwtStrategy({
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: process.env.JWT_SECRET
+  }, async function (jwtPayload, done){
+    try {
+      // const user = await
+    } catch (err) {
+      return done(new Error(), false)
+    }
   })
 )
 
